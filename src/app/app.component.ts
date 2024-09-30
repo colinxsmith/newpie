24aa5b6374a9a1e65144958e2b46b143b15f8431
Colin Smith <Colin.Smith@corfinancialgroup.com>
2024-09-30 12:13:02 +0100

    first change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cs_charts';
+  title = 'New Charts for Charles Stanley';
 }
