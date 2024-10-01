b7357e6bf77fb991dc2c9aebf0b6a17f9506d26e
Dr Colin Smith <colin.smith@corfinancialgroup.com>
2024-10-01 12:46:18 +0100

    .

--- src/app/twopiecharts/twopiecharts.component.ts
+++ src/app/twopiecharts/twopiecharts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as d3 from 'd3';
 
@@ -11,42 +11,62 @@ import * as d3 from 'd3';
 })
 export class TwopiechartsComponent implements OnInit {
   constructor(private element: ElementRef) { }
-  colours = d3.scaleLinear([0, 100], ['red', 'blue']);
+  colourRange=100;
+  colours = d3.scaleLinear([0, this.colourRange], ['red', 'blue']);
   paths: Array<string> = [];
+  centres:Array<Array<number>>=[];
   useColours: Array<string> = [];
+  @Input() title="Current Ranking Trevor";;
+  boxsize=400;
+  innerRadius= 10;
+  outerRadius= this.boxsize*0.8/2;
+  padAngle=0.02;
+  padRadius=100;
+  cornerRadius=20;
   mockdata = {
     'values': [4, 5, 3, 7, 1, 10, 17]
   };
   pie1 = d3.pie();
-  figureArcs = d3.arc();
+  figureArcs = d3.arc()
+  .padRadius(this.padRadius)
+  .padAngle(this.padAngle)
+  .cornerRadius(this.cornerRadius)
+  ;
   ngOnInit() {
     this.pie1(this.mockdata.values).forEach((s, i) => {
       this.paths.push(this.figureArcs({
-        innerRadius: 1,
-        outerRadius: 200,
-        padAngle: 0.005,
+        innerRadius: this.innerRadius,
+        outerRadius: this.outerRadius,
         startAngle: s.startAngle,
         endAngle: s.endAngle
       }) as string);
-      this.useColours.push(this.colours(100 * (s.index - 1) / this.mockdata.values.length));
+      const cent=this.figureArcs.centroid({
+        innerRadius: this.innerRadius,
+        outerRadius: this.outerRadius,
+        startAngle: s.startAngle,
+        endAngle: s.endAngle
+      });
+      this.centres.push(cent);
+      this.useColours.push(this.colours(this.colourRange * (s.index - 1) / this.mockdata.values.length));
       console.log(i, this.colours(100 * (s.index - 1) / this.mockdata.values.length), s)
     });
     this.update();
   }
   update() {
     setTimeout(()=>{
+      d3.select(this.element.nativeElement).selectAll('text.title')
+      .attr('x',this.boxsize/2-this.title.length*4)
+      .text(this.title);
       d3.select(this.element.nativeElement).selectAll('path.arc')
         .transition().duration(1000)
         .styleTween('opacity',()=>(t)=>`${t}`)
         .attrTween('d', (_, i) => (t) => {
-          //console.log(i);
           const p1 = this.pie1(this.mockdata.values)[i];
           const path = this.figureArcs({
             startAngle: p1.startAngle,
-            endAngle: p1.endAngle,
-            padAngle: p1.padAngle,
-            innerRadius: 1,
-            outerRadius: t * 150 +50
+            endAngle: p1.startAngle -t*(p1.startAngle-p1.endAngle),
+            innerRadius: this.innerRadius,
+            outerRadius: this.innerRadius-t*(this.innerRadius-this.outerRadius)
           }) as string;
           return `${path}`;
         });
