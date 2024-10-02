b89a84dd18990ddce0fb4c8b148c469fa268dd52
Colin Smith <colinxsmith@gmail.com>
2024-10-02 23:18:57 +0100

    Update barchart.component.ts

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-barchart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,AppBarchart],
   templateUrl: './barchart.component.html',
   styleUrl: './barchart.component.scss'
 })
