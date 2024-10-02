f2608724252e9328ce0649fc963c7dc390a73097
Colin <colinxsmith@gmail.com>
2024-10-02 17:13:31 +0100

    Tooltip shows up now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,6 @@ import { BarchartComponent } from "./barchart/barchart.component";
 })
 export class AppComponent {
   title = 'New Charts for Charles Stanley';
+  prop={'values':[4,5,17,15,9,10]};
+  curr={'values':[10,9,14,5,7,15]};
 }
--- src/app/twopiecharts/twopiecharts.component.ts
+++ src/app/twopiecharts/twopiecharts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef,Input } from '@angular/core';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as d3 from 'd3';
 
@@ -11,42 +11,45 @@ import * as d3 from 'd3';
 })
 export class TwopiechartsComponent implements OnInit {
   constructor(private element: ElementRef) { }
-  @Input() colourRange=100;
-  @Input() colourStart='red';
-  @Input() colourEnd='blue';
+  @Input() colourRange = 100;
+  @Input() colourStart = 'red';
+  @Input() colourEnd = 'blue';
   colours = d3.scaleLinear([0, 100], ['red', 'green']);
   paths: Array<string> = [];
-  centres:Array<Array<number>>=[];
+  centres: Array<Array<number>> = [];
   useColours: Array<string> = [];
-  @Input() title="Ranking Trevor";;
-  @Input() boxsize=400;
-  @Input() innerRadius= 10;
-  @Input() outerRadius= this.boxsize*0.8/2;
-  @Input() padAngle=0.02;
-  @Input() padRadius=100;
-  @Input() cornerRadius=20;
-  @Input() squareBorderOpacity=0;
-  mockdata = {
+  @Input() title = "Ranking Trevor";;
+  @Input() boxsize = 400;
+  @Input() innerRadius = 10;
+  @Input() outerRadius = this.boxsize * 0.8 / 2;
+  @Input() padAngle = 0.02;
+  @Input() padRadius = 100;
+  @Input() cornerRadius = 20;
+  @Input() squareBorderOpacity = 0;
+  @Input() mockdata = {
     'values': [4, 5, 3, 7, 1, 10, 17]
   };
   pie1 = d3.pie();
   figureArcs = d3.arc()
-  .padRadius(this.padRadius)
-  .padAngle(this.padAngle)
-  .cornerRadius(this.cornerRadius)
-  ;
+    .padRadius(this.padRadius)
+    .padAngle(this.padAngle)
+    .cornerRadius(this.cornerRadius)
+    ;
   ngOnInit() {
-    this.outerRadius= this.boxsize*0.8/2;
-    this.colours=d3.scaleLinear([0,this.colourRange],[this.colourStart,this.colourEnd])
-    console.log(this.colourStart,this.colourEnd,this.boxsize);
-    this.pie1(this.mockdata.values).forEach((s, i) => {
+    this.outerRadius = this.boxsize * 0.8 / 2;
+    this.colours = d3.scaleLinear([0, this.colourRange], [this.colourStart, this.colourEnd])
+    console.log(this.colourStart, this.colourEnd, this.boxsize);
+    
+    this.pie1(this.mockdata.values).forEach((s,ii) => {
+      const i=this.mockdata.values.length-1-  ii;
+      
       this.paths.push(this.figureArcs({
         innerRadius: this.innerRadius,
         outerRadius: this.outerRadius,
         startAngle: s.startAngle,
         endAngle: s.endAngle
       }) as string);
-      const cent=this.figureArcs.centroid({
+      const cent = this.figureArcs.centroid({
         innerRadius: this.innerRadius,
         outerRadius: this.outerRadius,
         startAngle: s.startAngle,
@@ -59,43 +62,51 @@ export class TwopiechartsComponent implements OnInit {
     this.update();
   }
   update() {
-    setTimeout(()=>{
+    setTimeout(() => {
       d3.select(this.element.nativeElement).selectAll('text.title')
-      .attr('x',this.boxsize/2-this.title.length*4)
-      .text(this.title);
+        .attr('x', this.boxsize / 2 - this.title.length * 4)
+        .text(this.title);
       d3.select(this.element.nativeElement).selectAll('path.arc')
         .transition().duration(1000)
-        .styleTween('opacity',()=>(t)=>`${t}`)
+        .styleTween('opacity', () => (t) => `${t}`)
         .attrTween('d', (_, i) => (t) => {
           const p1 = this.pie1(this.mockdata.values)[i];
           const path = this.figureArcs({
             startAngle: p1.startAngle,
-            endAngle: p1.startAngle -t*(p1.startAngle-p1.endAngle),
+            endAngle: p1.startAngle - t * (p1.startAngle - p1.endAngle),
             innerRadius: this.innerRadius,
-            outerRadius: this.innerRadius-t*(this.innerRadius-this.outerRadius)
+            outerRadius: this.innerRadius - t * (this.innerRadius - this.outerRadius)
           }) as string;
           return `${path}`;
         });
-    },200);
+    }, 200);
+  }
+  over(e: MouseEvent, i: number, inout = false) {
+    const svg = d3.select(this.element.nativeElement).select('div.mainTip');
+    const here = d3.select(e.target as HTMLElement & EventTarget);
+    console.log(e, i, svg, here);
+    if (inout) {
+      const x=e.clientX-100;
+      const y=e.clientY;
+      here.style('opacity',0.5);
+      svg
+        .attr('tiptitle', 'tipper')
+        .style('left',`${x}px`)
+        .style('top',`${y}px`)
+        .style('opacity', '1')
+        .html(`${x} ${y} i:${i} rank:${this.pie1(this.mockdata.values)[i].index} value :${this.mockdata.values[i]}`);
+      /* .style('--ff', '110%')
+       .style('--xx', xx + 'px')
+       .style('--yy', yy - this.height * 0.15 + 'px')
+       .style('--bx', e.offsetX + 'px')
+       .style('--by', yy + 'px')*/
+      ;
+    } else {
+      here.style('opacity',1);
+      svg.style('opacity', 0)
+        .attr('tiptitle', null);
+    }
   }
 }
 
 
-
-/*
-
-      svg
-        .attr('tiptitle', label)
-        .style('--back', 'black')
-        .style('--ff', '110%')
-        .style('--xx', xx + 'px')
-        .style('--yy', yy - this.height * 0.15 + 'px')
-        .style('--bx', e.offsetX + 'px')
-        .style('--by', yy + 'px')
-        ;
-    } else {
-      here.style('opacity', 1);
-      svg
-        .attr('tiptitle', null);
-    }
-        /
