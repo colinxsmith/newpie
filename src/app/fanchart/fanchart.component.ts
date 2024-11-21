ccc321369a0f3edde866638b5345e446de4a0d3b
Colin Smith <Colin.Smith@corfinancialgroup.com>
2024-11-21 16:37:48 +0000

    Keep this

--- src/app/fanchart/fanchart.component.ts
+++ src/app/fanchart/fanchart.component.ts
@@ -15,32 +15,53 @@ export class FanchartComponent implements OnInit {
   @Input() boxsizeH = 900;
   @Input() squareBorderOpacity = 1;
   @Input() DATA = {} as fchart;
-  maxY = 0;
+  @Input()title="fanchart";
+  maxY = -1e9;
   maxX = 0;
-  minY = 0;
+  minY = 1e9;
   minX = 0;
-  scaleX=d3.scaleLinear();
-  scaleY=d3.scaleLinear();
-  transform=(x:number,y:number,r=0)=>`translate(${x},${y}) rotate(${r})`;
+  scaleX = d3.scaleLinear();
+  scaleY = d3.scaleLinear();
+  intVal=(i:number)=>Math.floor(i);
+  plotPoints = (i: number, array: Array<number>, pos: number) => { };
+  transform = (x: number, y: number, r = 0) => `translate(${x},${y}) rotate(${r})`;
   constructor(private element: ElementRef) { }
   ngOnInit(): void {
     console.log(this.DATA);
     this.DATA.lines.forEach(d => {
-      this.minX = d3.min([this.minX, d3.min(d.values)??0])??0;
-      this.maxX = d3.max([this.maxX, d3.max(d.values)??0])??0;
+      this.minY = d3.min([this.minY, d3.min(d.values) ?? 0]) ?? 0;
+      this.maxY = d3.max([this.maxY, d3.max(d.values) ?? 0]) ?? 0;
     });
     this.DATA.areas.forEach(dd => {
       dd.forEach(d => {
-        this.minX = d3.min([this.minX, d3.min(d.values)??0])??0;
-        this.maxX = d3.max([this.maxX, d3.max(d.values)??0])??0;
+        this.minY = d3.min([this.minY, d3.min(d.values) ?? 0]) ?? 0;
+        this.maxY = d3.max([this.maxY, d3.max(d.values) ?? 0]) ?? 0;
       });
     });
-    this.maxY=this.DATA.areas.length+this.DATA.lines.length;
-    console.log(this.minX,this.maxX);
-    console.log(this.minY,this.maxY);
-    this.scaleX.domain([this.minX,this.maxX]);
-    this.scaleY.domain([this.minY,this.maxY]);
-    this.scaleX.range([5e-2*this.boxsizeH,95e-2*this.boxsizeH]);
-    this.scaleY.range([95e-2*this.boxsizeV,5e-2*this.boxsizeV]);
+    this.maxX = this.DATA.lines[0].values.length;
+    console.log(this.minX, this.maxX);
+    console.log(this.minY, this.maxY);
+    this.scaleX.domain([this.minX, this.maxX]);
+    this.scaleY.domain([this.minY, this.maxY]);
+    this.scaleX.range([15e-2 * this.boxsizeH, 95e-2 * this.boxsizeH]);
+    this.scaleY.range([90e-2 * this.boxsizeV, 10e-2 * this.boxsizeV]);
+    this.plotPoints = (i, values, pos) => {
+      let back = 0;
+      if (pos == 0) {
+        back = this.scaleX(i - 1);
+      } else if (pos == 1) {
+        back = this.scaleX(i)
+      } else if (pos == 2) {
+        back = this.scaleY(values[i - 1])
+      } else if (pos == 3) {
+        back = this.scaleY(values[i])
+      }
+      return back;
+    };
+    console.log(this.DATA.lines[0].values);
+    console.log(this.plotPoints(1, this.DATA.lines[0].values, 0),
+      this.plotPoints(1, this.DATA.lines[0].values, 1),
+      this.plotPoints(1, this.DATA.lines[0].values, 2),
+      this.plotPoints(1, this.DATA.lines[0].values, 3));
   }
 }
