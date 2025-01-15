1c66c65a15a9d543936a409d08bec0c9a1f719cf
Colin <colinxsmith@gmail.com>
2025-01-15 17:46:03 +0000

    tips

--- src/app/histogram/histogram.component.ts
+++ src/app/histogram/histogram.component.ts
@@ -13,16 +13,44 @@ export class HistogramComponent implements OnInit {
   @Input() DATA = {};
   boxsizeV = 900;
   boxsizeH = 900;
+  rim = 10;
   scaleX = d3.scaleLinear();
   scaleY = d3.scaleLinear();
   heightToWidth = 0.35;
   title = 'Response Histogram';
+  staticPosition = { x: '2.5%', y: '5%' };
   squareBorderOpacity = 1;
   ngOnInit(): void {
     console.log(this.DATA)
     setTimeout(() => {
       this.newDimensions();
     }, 0);
+  }
+  divover(s: MouseEvent, newtitle = '', inside = false) {
+    const area = d3.select(this.element.nativeElement).select('#histo');
+    const x1 = this.staticPosition.x;
+    const y1 = this.staticPosition.y;
+    const text1 = this.title;
+    console.log(x1, y1);
+    if (inside) {
+      const x = s.offsetX;
+      const y = s.offsetY;
+      area
+        .style('--xx', `${x}px`)
+        .style('--yy', `${y}px`)
+        .style('--trans','translate(-50%,0)')
+        .attr('rogue-title', newtitle)
+        ;
+      console.log(x, y, newtitle)
+    }
+    else {
+      area
+        .style('--xx', `${x1}`)
+        .style('--yy', `${y1}`)
+        .style('--trans','translate(0,0)')
+        .attr('rogue-title', text1)
+        ;
+    }
   }
   newDimensions() {
     const drawthis = d3.select(this.element.nativeElement);
@@ -31,11 +59,12 @@ export class HistogramComponent implements OnInit {
     // const boxsizeV = ((drawthis.select('#histo').node() as HTMLElement).parentElement?.parentElement?.parentElement as HTMLElement).getBoundingClientRect().height;//Math.max(boxsizeH * this.heightToWidth, here.height);
     const boxsizeH = here.width;
     const boxsizeV = Math.max(boxsizeH * this.heightToWidth, here.height);
-    this.boxsizeH = boxsizeH;
-    this.boxsizeV = boxsizeV;
     drawthis.select('svg')
+      .attr('width', boxsizeH)
       .attr('height', boxsizeV)
-      .attr('width', boxsizeH);
+      ;
+    this.boxsizeH = boxsizeH - this.rim;
+    this.boxsizeV = boxsizeV - this.rim;
 
     console.log('Box dimensions:', boxsizeH, boxsizeV)
   }
